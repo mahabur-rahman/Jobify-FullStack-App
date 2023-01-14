6c78eb81203a7b7be7a18f20297b8cc9f63ddd8f
mahabur <annur4395@gmail.com>
2023-01-14 17:30:26 +0600

    pagination backedn

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -66,11 +66,20 @@ const getAllJobs = async (req, res) => {
     result = result.sort("-position"); // desc
   }
 
+  // pagination 👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
 
-  return res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  return res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 // update job
